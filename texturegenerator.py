0576a38ce71b9e990b1c74cba6b5e0c7f3970ec8
WOKNz <sashafix@gmail.com>
2020-04-12 22:25:30 +0300

    Perspective transformation added with rotation matrix and shift (Z axis)

--- texturegenerator.py
+++ texturegenerator.py
@@ -2,6 +2,9 @@
 from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
+from scipy.spatial.transform import Rotation as R
+import pandas as pd  # better printouts
 
 
 def dots_texture_2d(size_of_circle, size_of_bkg_square, grid):
@@ -34,18 +37,59 @@ def dots_texture_2d(size_of_circle, size_of_bkg_square, grid):
 	return stacked_img
 
 
-def rotate_imgae_in_3d(img, omega, phi, kappa, shift):
+def transfom(img_in, omega=None, phi=None, kappa=None, scale=None):
 	"""
 
-	:param img: tiff image of texture
-	:param omega: in radians
-	:param phi: in radians
-	:param kappa: in radians
-	:param shift: tuple (dx,dy) from where?
-	:return: rotated and shifted image
+	:param img: PIL img
+	:param omega: in degrees
+	:param phi: in degrees
+	:param kappa: in degrees
+	:param scale: float
+	:return: rotated and scaled image
 	"""
 	# Implementation
-	pass
+	img = cv2.cvtColor(np.array(img_in), cv2.COLOR_RGB2BGR)  # Convert to CV2 img type
+	rows, cols = img.shape[:2]  # Max corner of the img
+
+	r = R.from_euler('xyz', (omega, phi, kappa), degrees=True)  # Rotation Matrix
+	# print(r.as_euler('xyz', degrees=True))
+	# print(r.as_dcm())
+
+	src_points = np.float32([[0, 0], [cols - 1, 0], [0, rows - 1], [cols - 1, rows - 1]])  # Original Corners of the img
+	rotation_mat = r.as_dcm() * 1  # Convert to np array
+	points = np.hstack((src_points[:, 0].reshape(4, 1) - int(cols / 2), src_points[:, 1].reshape(4, 1) - int(rows / 2)))
+	points = np.vstack((points.T, np.zeros((1, src_points.shape[0]))))
+	dst_points = scale * np.dot(rotation_mat, points)
+
+	# perspective on the image plane (corners)
+	planeNormal = np.array([0, 0, 1])
+	planePoint = np.array([0, 0, 0])
+	rayPoint = np.array([0, 0, (img_in.width + img_in.height) * 2])  # Perspective point behind the plane of the img
+
+	for i in range(4):  # Calculating the point on the img plane
+		dst_points[:, i] = LinePlaneCollision(planeNormal, \
+		                                      planePoint, \
+		                                      dst_points[:, i] + np.array([0, 0, (img_in.width + img_in.height) * 2]), \
+		                                      rayPoint)
+
+	dst_points[0, :] = dst_points[0, :] + int(cols / 2)
+	dst_points[1, :] = dst_points[1, :] + int(rows / 2)
+	dst_points = np.float32(dst_points[0:2, :].T)
+	# print(pd.DataFrame(dst_points))
+	projective_matrix = cv2.getPerspectiveTransform(src_points, dst_points)  # Transformation matrix
+	output = cv2.warpPerspective(img, projective_matrix, (cols, rows))  # Transformed image
+	return Image.fromarray(output)
+
+
+def LinePlaneCollision(planeNormal, planePoint, rayDirection, rayPoint, epsilon=1e-12):
+	ndotu = planeNormal.dot(rayDirection)
+	if abs(ndotu) < epsilon:
+		raise RuntimeError("no intersection or line is within plane")
+
+	w = rayPoint - planePoint
+	si = -planeNormal.dot(w) / ndotu
+	Psi = w + si * rayDirection + planePoint
+	return Psi
 
 
 # Test area
@@ -61,5 +105,23 @@ for i in range(len(square_sizes)):
 fig.text(0.5, 0.015, 'Change in Initial Square Size', ha='center', va='center')
 fig.text(0.021, 0.5, 'Change in Initial Circle Size', ha='center', va='center', rotation='vertical')
 plt.tight_layout()
-plt.savefig('CompareOfRamaters.png', dpi=300)
+plt.savefig('CompareOfPamaters.png', dpi=300)
 plt.show()
+
+# # Testing 2D transformed texture
+omegas = [5, 10, 20, 40, 80]
+phis = [5, 10, 20, 40, 80]
+# phis = [5, 10, 20, 40, 80]
+
+fig, axes = plt.subplots(5, 5)
+for i in range(len(omegas)):
+	for j in range(len(phis)):
+		rotated = transfom(dots_texture_2d(25, 75, (8, 8)), omegas[i], phis[j], 0, 0.8)
+		axes[i, j].imshow(rotated, cmap='gray')
+fig.text(0.5, 0.015, 'Change in Phi Angle', ha='center', va='center')
+fig.text(0.021, 0.5, 'Change in Omega Angle', ha='center', va='center', rotation='vertical')
+plt.tight_layout()
+plt.savefig('CompareOfAngles.png', dpi=300)
+plt.show()
+
+Image.Image.show(transfom(dots_texture_2d(25, 75, (8, 8)), 35, 13, 8, 0.6))
